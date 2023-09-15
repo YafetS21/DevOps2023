8030cf638b05a924f92dfd275d18817a62c2c087
yafet <yafetsebhat@hotmail.com>
2023-09-15 19:14:50 +0200

    first commit

---
+++ total = 0.py
@@ -0,0 +1,8 @@
+print("Vad vill du äta?")
+äta = input()
+svar = 'Vad gott det låter med att äta ' + äta + '!'
+print(svar)
+
+print("Vill du ha något att dricka?")
+dricka = input()
+print(vad bra kombo äta + dricka)
---
+++ Övningsfrågor.py
@@ -0,0 +1,10 @@
+def main():
+    print("Hallå!")
+    print("Vad är ditt förnamn?")
+    fornamn = input("du > ")
+    print("Vad är ditt efternamn?")
+    efternamn = input("du > ")
+    print("dator > Trevligt att träffas {fornamn} {efternamn}!")
+
+if __name__ == "__main__":
+    main()
